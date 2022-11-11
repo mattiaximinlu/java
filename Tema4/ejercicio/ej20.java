2124bd004f137fb8f1125bf0def76a56f3bb4794
mattiaximinlu <mlu481@g.educaand.es>
2022-11-11 14:31:58 +0100

    1

--- Tema4/ejercicio/ej20.java
+++ Tema4/ejercicio/ej20.java
@@ -12,23 +12,16 @@ public class ej20 {
         int pos1=h-1;
         int pos2=h-1;
         System.out.println("------------------------------------------------");
-        for (int nFila = 0; nFila < h; nFila++) {
-            if (nFila==h-1) {
-                for (int nColumna = 0; nColumna < h*2-1; nColumna++) {
-                    System.out.print(caracter);
-                }
-            } else {
-                for (int nColumna = 0; nColumna < h*2; nColumna++) {
-                    if (pos1==nColumna || pos2==nColumna) {
-                        System.out.print(caracter);
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-                pos1=pos1-1;
-                pos2=pos2+1;
-                System.out.println(" ");
+        for (int nFila = 0; nFila < h-1; nFila++) {
+            for (int nColumna = 0; nColumna < h*2; nColumna++) {
+                System.out.print(pos1==nColumna || pos2==nColumna?caracter:" ") ;
             }
+            pos1=pos1-1;
+            pos2=pos2+1;
+            System.out.println();
+        }
+        for (int base = 0; base < h*2-1; base++) {
+            System.out.print(caracter);
         }
         System.out.println();
     }
