c6a0fc6d9c26f5529779d0caddbb8b5efb0e70ad
mattiaximinlu <mlu481@g.educaand.es>
2022-11-11 13:53:13 +0100

    1

--- Concurso/San_Fermines.java
+++ Concurso/San_Fermines.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 public class San_Fermines {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        do {
-            int nToros=sc.nextInt();
-            if (nToros==0){
-                break;
-            }else{
-                int max=0;
-                for (int i = 0; i < nToros; i++) {
-                    int velocidad=sc.nextInt();
-                    System.out.println(i);
-                    max=velocidad>max?velocidad:max;
+        for (int i = 0; i < 2; i++) {
+            String[] num=sc.nextLine().split(" ");
+            int pos=0;
+            int max=0;
+            for (String string : num) {
+                int integer=Integer.valueOf(string);
+                if (pos==0) {
+                    pos=pos+1;
+                } else if (integer>max && integer<1000000000 && integer>0){
+                    max=integer;
                 }
-                System.out.println(max);
             }
-                
-        } while (true);
+            System.out.println(max);    
+        }
         sc.close();
     }
 }
