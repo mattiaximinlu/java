ab9bb3242a9432fb84c6f84b192b191364ffba59
mattiaximinlu <mlu481@g.educaand.es>
2022-11-24 10:57:25 +0100

    1

--- Tema4/mas ej/ej4.java
+++ Tema4/mas ej/ej4.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 public class ej4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Factor: ");
-        int factor=sc.nextInt();
-        sc.nextLine();
-        //simulacion de numeros muy muy grandes y que dé muchas vueltas
-        factor=factor%26;
         System.out.print("frase: ");
         String frase=sc.nextLine();
         sc.close();
         String cifrado="";
         for (int i = 0; i < frase.length(); i++) {
-            char letra=(char)(frase.charAt(i)+factor);
+            int factor=(int)(Math.random()*26);
+            char letraPos=frase.charAt(i);
+            char letra=(char)(letraPos+factor);
             //comprobar que la letra esté entre A y Z y si una vez sumado supera la Z resta de 26, lo mismo con las minusculas
-            if ((frase.charAt(i)>65 && frase.charAt(i)<65+26 &&letra>65+26)||(frase.charAt(i)>97 && frase.charAt(i)<97+26 &&letra>97+26)) {
+            if ((letraPos>='A' && letraPos<='Z' &&letra>'Z')||(letraPos>='a' && letraPos<='z' &&letra>='z')) {
                 letra-=26;
             }
-            cifrado+=letra;
+            cifrado+=factor+""+letra;
         }
-        System.out.println(factor+cifrado);
+        System.out.println(cifrado);
     }
 }
