9332e36d78d2733e7e5423d25f0647afe7342d01
mattiaximinlu <mlu481@g.educaand.es>
2023-01-09 08:38:32 +0100

    1

--- Tema5/ejercicio/ej4.java
+++ Tema5/ejercicio/ej4.java
@@ -1,15 +1,15 @@
 
 public class ej4 {
     public static void main(String[] args) {
-        int[] numero=new int[20];
-        int[] cuadrado=new int[20];
-        int[] cubo=new int[20];
+        int[] numero = new int[20];
+        int[] cuadrado = new int[20];
+        int[] cubo = new int[20];
         for (int i = 0; i < numero.length; i++) {
-            int random=(int)(Math.random()*100);
-            numero[i]=random;
-            cuadrado[i]=(int)(Math.pow(numero[i],2));
-            cubo[i]=(int)(Math.pow(numero[i],3));
-            System.out.print(numero[i]+"\t"+cuadrado[i]+"\t"+cubo[i]);
+            int random = (int) (Math.random() * 100);
+            numero[i] = random;
+            cuadrado[i] = (int) (Math.pow(numero[i], 2));
+            cubo[i] = (int) (Math.pow(numero[i], 3));
+            System.out.print(numero[i] + "\t" + cuadrado[i] + "\t" + cubo[i]);
             System.out.println();
         }
     }
