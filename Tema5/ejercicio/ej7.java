f4247d66e3223ec9931d21acf5d69ebd7801d20f
mattiaximinlu <mlu481@g.educaand.es>
2023-01-09 09:47:53 +0100

    1

--- Tema5/ejercicio/ej7.java
+++ Tema5/ejercicio/ej7.java
@@ -2,25 +2,36 @@ import java.util.Scanner;
 
 public class ej7 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        //creacion de la lista
         int[] lista=new int[100];
         for (int i = 0; i < lista.length; i++) {
             lista[i]=(int)(Math.random()*21);
             System.out.print(lista[i]+" ");
         }
+
+        //pedir valores
         System.out.println();
+        Scanner sc=new Scanner(System.in);
         System.out.print("Que numero quieres cambiar? ");
         int antes=sc.nextInt();
-        System.out.print("Por que quieres cambiar?");
+        System.out.print("Por que quieres cambiar? ");
         int despues=sc.nextInt();
         sc.close();
+
+        //imprimir la lista sustituyendo los valores
         for (int i = 0; i < lista.length; i++) {
-            if (lista[i]==antes) {
+            //cambiar los valores
+
+            /* if (lista[i]==antes) {
                 lista[i]=despues;
                 System.out.print("\""+lista[i]+"\" ");
             } else {
                 System.out.print(lista[i]+" ");
-            }
+            } */
+
+
+            //imprimir el otro valor sin cambiarlo
+            System.out.print(lista[i]==antes?"\""+despues+"\" ":lista[i]+" ");
         }
         System.out.println();
     }
